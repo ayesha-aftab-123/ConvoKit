rgangela99/Cornell-Conversational-Analysis-Toolkit

add random sampling for entropy calculation

setup.py
@@ -26,6 +26,7 @@ setup(
                 "convokit.text_processing",
                 "convokit.speaker_convo_helpers",
                 "convokit.speakerConvoDiversity",
+                "convokit.surprise",
               ],
     package_data={"convokit": ["data/*.txt"]},
     install_requires=[

ayesha-aftab-123/ConvoKit

setup.py
@@ -27,7 +27,8 @@ setup(
                 "convokit.text_processing",
                 "convokit.speaker_convo_helpers",
                 "convokit.speakerConvoDiversity",
-                "convokit.expected_context_framework"
+                "convokit.expected_context_framework",
+                "convokit.surprise"
               ],
     package_data={"convokit": ["data/*.txt", "politeness_local/lexicons/*.json"]},
     install_requires=[
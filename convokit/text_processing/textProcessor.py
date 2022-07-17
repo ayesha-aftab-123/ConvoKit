5b90be776b4ddc8da9b6295b2463eafb0929175b
Oscar <os2304@columbia.edu>
2022-07-17 09:29:28 +0800

    revert

--- convokit/text_processing/textProcessor.py
+++ convokit/text_processing/textProcessor.py
@@ -52,7 +52,7 @@ class TextProcessor(Transformer):
         :return: the corpus
         """
 
-        total_utts = len(corpus.utterances)
+        total_utts = len(corpus.iter_utterances())
 
         for idx, utterance in enumerate(corpus.iter_utterances()):
 
